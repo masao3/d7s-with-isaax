bd2853eaf05d86c236473ad8cd48c5ff1a3f2dad
masao3 <46898480+masao3@users.noreply.github.com>
2019-02-27 20:42:22 +0900

    Update main.py
    sample

--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 # coding: utf-8
+# Sample that outputs the value acquired by D7S.
 from __future__ import print_function
 import os
 import time
 import datetime
 import grove_d7s
-import ambient
+from ambient_connector import AmbientConnector
 # sensor instance
 sensor = grove_d7s.GroveD7s()
-# ambient instance
-try:
-    AMBIENT_CHANNEL_ID = int(os.environ['AMBIENT_CHANNEL_ID'])
-    AMBIENT_WRITE_KEY = os.environ['AMBIENT_WRITE_KEY']
-    am = ambient.Ambient(AMBIENT_CHANNEL_ID, AMBIENT_WRITE_KEY)
-except KeyError:
-    print("isaaxの環境変数サービスを使って AMBIENT_CHANNEL_ID と AMBIENT_WRITE_KEY を設定してください")
-    exit(1)
+# ambientをラップしたAmbientConnectorを使います
+am = AmbientConnector()
 def main():
     while sensor.isReady() == False:
         print('.')
         time.sleep(1.0)
     print("start")
     while True:
-        # 10秒のインターバルを設定
-        time.sleep(10)
+        # 1秒間隔でデータを取得する
+        time.sleep(1.0)
+        
         # センサーデータの取得
         si = sensor.getInstantaneusSI()
         pga = sensor.getInstantaneusPGA()
         now = datetime.datetime.today()
         eq = sensor.isEarthquakeOccuring()
+        # デバッグ用にデータを標準出力（本当は不要）
+        print(now.strftime("[%Y/%m/%d %H:%M:%S]"),
+            "SI={}[Kine]".format(si), 
+            "PGA={}[gal]".format(pga),
+            "EQ=%s" % eq)
         # センサーの初期化中は値がNoneになるので処理をスキップ
         if si == None and pga == None:
             continue
@@ -38,14 +39,7 @@ def main():
             "d2": pga,
             "created": now.strftime("%Y/%m/%d %H:%M:%S")
             }
-        try:
-            am.send(payload)
-        except Exception as e:
-            print(e)
-        # デバッグ用に送信したデータを標準出力（本当は不要）
-        print(now.strftime("[%Y/%m/%d %H:%M:%S]"),
-            "SI={}[Kine]".format(si), 
-            "PGA={}[gal]".format(pga),
-            "EQ=%s" % eq)
+        # データをバッファーする
+        am.buffer(payload)
 if __name__ == '__main__':
     main()
